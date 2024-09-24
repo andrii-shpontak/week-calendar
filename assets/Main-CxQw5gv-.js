43569d74e9b306dd07d073ce53ab6d978467477e
andrii-shpontak <andrii-shpontak@users.noreply.github.com>
2024-09-24 15:43:38 +0000

    deploy: 06ed788d3ad456d0d5a5db69fe1295e070d28000

--- assets/Main-CxQw5gv-.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-BNsroBu6.js";const a={};function o(r,t){return n(),c("h2",null,"Main page")}const _=e(a,[["render",o]]);export{_ as default};
--- assets/Scheduler-B7owJps-.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./index-BNsroBu6.js";const n={};function o(t,a){return r(),c("h2",null,"Scheduler page")}const _=e(n,[["render",o]]);export{_ as default};
