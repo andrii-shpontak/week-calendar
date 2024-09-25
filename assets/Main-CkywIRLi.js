cb706b44d01f46f02592159ef174e81b5e6389d8
andrii-shpontak <andrii-shpontak@users.noreply.github.com>
2024-09-25 06:54:36 +0000

    deploy: ad4b481a28a6a9371bac74298786a007f636962f

--- assets/Main-CkywIRLi.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,a as e,b as a,o as n}from"./index-DjEA44wn.js";const r={},c={class:"text-center"};function l(d,t){return n(),s("div",c,t[0]||(t[0]=[e("h2",{class:"font-bold text-lg"},"Main page",-1),e("p",null,[a("I haven't figured out what will happen here yet."),e("br"),a("Please go to the SCHEDULER page")],-1)]))}const p=o(r,[["render",l]]);export{p as default};
